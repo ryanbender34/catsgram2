efc0ac9dadff7268ae997e0d494e954970f13b9f
Rick Arocho <rick.arocho@gmail.com>
2021-09-22 15:39:34 -0500

    phase 2

--- practice-for-week-09-catsagram-long-practice-day-2/public/main.js
+++ practice-for-week-09-catsagram-long-practice-day-2/public/main.js
@@ -14,21 +14,84 @@ export const createMainContent = () => {
     button.innerText = 'Next Cat'
     button.style.padding = "10px 20px";
 
+    //create btn container
+
+    const btnContainer = document.createElement('div');
+    btnContainer.style.display = 'flex';
+    btnContainer.style.flexDirection = 'row-reverse';
+    btnContainer.style.gap = '12px';
+
+    //create upVote btn
+
+    const upButton = document.createElement('button');
+    upButton.setAttribute('class', 'up-btn');
+    upButton.innerText = 'Up Vote';
+    upButton.style.padding = "6px 10px";
+    upButton.style.marginBottom = '14px';
+    upButton.style.backgroundColor = 'green';
+    upButton.style.color = 'white';
+    upButton.style.border = 'none';
+    upButton.style.borderRadius = '4px';
+
+    btnContainer.appendChild(upButton);
+
+    //create downVote btn
+
+    const downButton = document.createElement('button');
+    downButton.setAttribute('class', 'down-btn');
+    downButton.innerText = 'Down Vote';
+    downButton.style.padding = "6px 10px";
+    downButton.style.marginBottom = '14px';
+    downButton.style.backgroundColor = 'red';
+    downButton.style.color = 'white';
+    downButton.style.border = 'none';
+    downButton.style.borderRadius = '4px';
+
+    btnContainer.appendChild(downButton);
+
+    //Create vote counter
+
+    let voteCounter = 0;
+    const count = document.createElement('p');
+    count.innerText = 'Popularity Score: ' + voteCounter;
+
+    //Up vote count
+
+    upButton.addEventListener('click', e => {
+        console.log('test');
+        voteCounter++;
+        count.innerText = 'Popularity Score: ' + voteCounter;
+        console.log(voteCounter);
+    });
+
+    //Down vote count
+
+    downButton.addEventListener('click', e => {
+        voteCounter--;
+        count.innerText = 'Popularity Score: ' + voteCounter;
+    });
+
     //container
     const container = document.querySelector(".container");
     container.appendChild(h1);
     container.appendChild(button);
     container.appendChild(img);
+    container.appendChild(count);
+    container.appendChild(btnContainer);
 
     //fetch next image
     button.addEventListener('click', e => {
         fetchImage();
+
+        voteCounter = 0;
+        count.innerText = 'Popularity Score: ' + voteCounter;
     })
 
     fetchImage();
 
 
 };
+
 
 const fetchImage = async () => {
     // Fetch image from API and set img url
